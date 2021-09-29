0d2b70d9dd8280326b61f9add38ab7928b63d3d9
Mauricio Romero <mauricionromero@hotmail.com>
2021-09-29 11:51:47 -0300

    change about view to github profile

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 
-# its-raining
+# Its Raining
 React based weather seeker
+
+Live: [Here!](https://its-raining.vercel.app)
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,8 @@ import { StyledFooter } from "./StyledComponents";
 export default function Footer() {
   return (
     <StyledFooter>
-      <NavLink to="/about">About</NavLink>
+      <a href="https://github.com/TanisJam" target="_blank"> 2021 - Mauricio Romero</a>
+      {/* <NavLink to="/about">About</NavLink> */}
     </StyledFooter>
   );
 }
